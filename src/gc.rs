279603f30dd6f14ac24f1f92666c3b73a3c6d34c
Julian Rosse <julian@helixbass.net>
2022-12-18 17:44:47 -0500

    allow unsafe

--- src/gc.rs
+++ src/gc.rs
@@ -1,8 +1,9 @@
-use crate::{IndexMap, IndexSet};
+use crate::IndexMap;
 use gc::{custom_trace, Finalize, Trace};
 use std::hash::{BuildHasher, Hash};
 
 impl<K: Eq + Hash + Trace, V: Trace, S: BuildHasher> Finalize for IndexMap<K, V, S> {}
+#[allow(unsafe_code)]
 unsafe impl<K: Eq + Hash + Trace, V: Trace, S: BuildHasher> Trace for IndexMap<K, V, S> {
     custom_trace!(this, {
         for (k, v) in this.iter() {
